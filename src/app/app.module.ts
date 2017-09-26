e00b36909f4a1bfdc12bbcf9f27347f9a9741955
Michael Long <mlong@captechventures.com>
2017-09-25 20:46:27 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http';
+import { MdToolbarModule, MdIconModule, MdTableModule, MdSnackBarModule, MdSortModule, MdButtonModule, MdFormFieldModule, MdInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { TableComponent } from './components/table/table.component';
+import { DataService } from 'app/services/data.service';
+import { StorageService } from 'app/services/storage.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    MdTableModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdSnackBarModule,
+    MdSortModule,
+    MdButtonModule,
+    MdFormFieldModule,
+    MdInputModule
   ],
-  providers: [],
+  providers: [DataService, StorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
